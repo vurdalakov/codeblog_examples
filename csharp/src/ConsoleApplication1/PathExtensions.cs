90da3afe0b44dbaa81545fba70a7817bb02bbd44
vurdalakov <vurdalakov@gmail.com>
2013-11-21 08:33:22 +0200

    GetShortPathName and GetLongPathName

---
+++ csharp/src/ConsoleApplication1/PathExtensions.cs
@@ -0,0 +1,40 @@
+﻿namespace Vurdalakov
+{
+    using System;
+    using System.IO;
+    using System.Runtime.InteropServices;
+    using System.Text;
+
+    public static class PathExtensions
+    {
+        public static String GetShortPathName(String longPath)
+        {
+            StringBuilder shortPath = new StringBuilder(longPath.Length + 1);
+
+            if (0 == PathExtensions.GetShortPathName(longPath, shortPath, shortPath.Capacity))
+            {
+                return longPath;
+            }
+            
+            return shortPath.ToString();
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        private static extern Int32 GetShortPathName(String path, StringBuilder shortPath, Int32 shortPathLength);
+
+        public static String GetLongPathName(String shortPath)
+        {
+            StringBuilder longPath = new StringBuilder(1024);
+
+            if (0 == PathExtensions.GetLongPathName(shortPath, longPath, longPath.Capacity))
+            {
+                return shortPath;
+            }
+
+            return longPath.ToString();
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        private static extern Int32 GetLongPathName(String shortPath, StringBuilder longPath, Int32 longPathLength);
+    }
+}
--- csharp/src/ConsoleApplication1/Program.cs
+++ csharp/src/ConsoleApplication1/Program.cs
@@ -1,12 +1,22 @@
 ﻿namespace Vurdalakov
 {
     using System;
+    using System.IO;
 
     class Program
     {
         static void Main(string[] args)
         {
-            StringCollection2IEnumerableString.StringCollection2List();
+//            StringCollection2IEnumerableString.StringCollection2List();
+
+            String path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles), @"Internet Explorer\iexplore.exe");
+            Console.WriteLine("Long path:  '{0}'", path);
+
+            path = PathExtensions.GetShortPathName(path);
+            Console.WriteLine("Short path: '{0}'", path);
+            
+            path = PathExtensions.GetLongPathName(path);
+            Console.WriteLine("Long path:  '{0}'", path);
         }
     }
 }
